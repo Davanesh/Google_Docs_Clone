dabffc77654c9bfb67fd7a8803ea64016c7ce781
Davanesh <davaneshsaminathan335@gmail.com>
2024-12-14 22:17:56 +0530

    file name

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -42,8 +42,13 @@ import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Avatars } from "./avatars";
 import { Inbox } from "./inbox";
+import { Doc } from "../../../../convex/_generated/dataModel";
 
-export const Navbar = () => {
+interface NavbarProps {
+  data: Doc<"documents">
+};
+
+export const Navbar = ({ data }: NavbarProps) => {
   const {editor} = useEditorStore();
   const insertTable = ({rows, cols}: { rows: number, cols: number }) => {
     editor
@@ -67,7 +72,7 @@ export const Navbar = () => {
     const blob = new Blob([JSON.stringify(content)], {
       type: "application/json",
     });
-    onDownload(blob, `document.json`)
+    onDownload(blob, `${data.title}.json`)
   };
 
   const onSaveHTML = () => {
@@ -76,7 +81,7 @@ export const Navbar = () => {
     const blob = new Blob([content], {
       type: "text/html",
     });
-    onDownload(blob, `document.html`)
+    onDownload(blob, `${data.title}.html`)
   };
 
   const onSaveText = () => {
@@ -85,7 +90,7 @@ export const Navbar = () => {
     const blob = new Blob([content], {
       type: "text/plain",
     });
-    onDownload(blob, `document.txt`)
+    onDownload(blob, `${data.title}.txt`)
   };
 
   return (
@@ -95,7 +100,7 @@ export const Navbar = () => {
           <Image src="/logo.svg" alt="logo" width={32} height={36}/>
         </Link>
         <div className="flex flex-col">
-          <DocumentInput/>
+          <DocumentInput title={data.title} id={data._id}/>
           <div className="flex">
             <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
               <MenubarMenu>
